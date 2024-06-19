aebd2c524d6358b637420aa01ceac1e252e4e5ff
uh-sman <75017192+uh-sman@users.noreply.github.com>
2024-06-19 02:58:49 -0700

    Delete .history/src directory

--- .history/src/components/cloudinary/Input_20231203064427.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Input = ({title, placeholder, setForm, form, type}) => {
-  return <input title={title} type={type} placeholder={placeholder} onChange={(e) => setForm({...form, name: e.target.value})}/>
-}
-
-export default Input
--- .history/src/components/cloudinary/Input_20231203065524.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Input = ({title, placeholder, setForm, form, type, onChange}) => {
-  return <input title={title} type={type} placeholder={placeholder} onChange={onchange}/>
-}
-
-export default Input
